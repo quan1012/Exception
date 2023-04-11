3739701bd412cf293e463f872ce4310e38a569c3
quan1012 <quanquanquan10121999@example.com>
2023-04-11 11:53:29 +0700

    Ngoại lệ

---
+++ src/ArrayExample.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+    public Integer[] createRandom() {
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("Danh sách phần tử của mảng: ");
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.println(arr[i] + " ");
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] arr = arrayExample.createRandom();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của 1 phần tử bất kì: ");
+        int x = scanner.nextInt();
+        try{
+            System.out.println("Gía trị của phần tử chỉ số " + x + " là " + arr[x] );
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cạnh 1: ");
+        int a = scanner.nextInt();
+        System.out.println("Cạnh 2: ");
+        int b = scanner.nextInt();
+        System.out.println("Cạnh 3: ");
+        int c = scanner.nextInt();
+        try{
+            checlTriangle(a, b, c);
+            System.out.println("Đây là tam giác");
+        } catch (Exception e){
+            System.out.println("Lỗi: " + e.getMessage());
+        }
+    }
+    public static void checlTriangle( int a, int b, int c) throws Exception{
+        if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || b + c <= a || c + a <= b){
+            throw new Exception("Ko là tam giác");
+        }
+    }
+
+}
